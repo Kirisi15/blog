fc27b06704471d23e86f686a3b5d583451fd0576
Kirisi15 <kkirisigan@gmail.com>
2025-06-12 20:51:43 +0530

    modified header with links to direct pages

---
+++ blogapp/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css"
+import React from 'react';
+import myImage from "../../assets/4.jpg";
+
+
+export const Header = () => {
+  return (
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headerTitleSm">React & Node</span>
+            <span className="headerTitleLg">Blog</span>
+        </div>
+        <img className="headerImg" src={myImage} alt="" />
+    </div>
+  )
+}
+
+export default Header;
